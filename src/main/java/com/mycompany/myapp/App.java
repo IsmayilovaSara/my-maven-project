2bf1c9bca72895744ed28d25cbc2849cdb361e41
IsmayilovaSara <ismayilovassara@gmail.com>
2025-02-17 22:44:03 +0400

    Add project

---
+++ src/main/java/com/mycompany/myapp/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+
+    public String getGreeting() {
+        return "Hello, Maven!";
+    }
+}
+
